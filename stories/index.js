59c6feb4fcc03ae5f20437f2ac623291a4e788f9
Tom Adam <tom.adam21@gmail.com>
2020-10-26 12:45:30 -0700

    button working and tested

--- stories/index.js
+++ stories/index.js
@@ -9,11 +9,19 @@ import Button from "components/Button";
 
 storiesOf("Button", module)
   .addParameters({
-    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
-  .add("Base", () => <Button>Base</Button>)
-  .add("Confirm", () => <Button confirm>Confirm</Button>)
-  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Base", () => <Button onClick={action("button-clicked")}>Base</Button>)
+  .add("Confirm", () => (
+    <Button onClick={action("button-clicked")} confirm>
+      Confirm
+    </Button>
+  ))
+  .add("Danger", () => (
+    <Button onClick={action("button-clicked")} danger>
+      Cancel
+    </Button>
+  ))
   .add("Clickable", () => (
     <Button onClick={action("button-clicked")}>Clickable</Button>
   ))
