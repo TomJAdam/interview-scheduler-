eaa39c7db03540d886e28f145d9bff36251973e7
Tom Adam <tom.adam21@gmail.com>
2020-11-04 18:10:48 -0800

    added validation to form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -24,6 +24,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (!interviewer) {
+      setError("Interviewer must be selected");
+      return;
+    }
     setError("");
     props.onSave(name, interviewer);
   };
